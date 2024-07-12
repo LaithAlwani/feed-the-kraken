4c3802684bbee0957b9afdfb4888506f33d0966a
laith <laithalwani@gmail.com>
2024-07-12 13:50:40 -0400

    testing pusher client with vercel

---
+++ app/api/message/route.js
@@ -0,0 +1,9 @@
+import { pusherServer } from '@/lib/pusher'
+
+export async function POST(req) {
+  const  input  = await req.json()
+
+  pusherServer.trigger("124",'incoming-message', input)
+  
+  return  Response.json(({ status:200, success: true }))
+}
