9146a0a769b1e1a5168fb09657c7f7b7f84a8e24
laith <laithalwani@gmail.com>
2024-07-13 09:44:34 -0400

    pusher fix

--- app/api/message/route.js
+++ app/api/message/route.js
@@ -3,7 +3,7 @@ import { pusherServer } from '@/lib/pusher'
 export async function POST(req) {
   const  input  = await req.json()
 
-  pusherServer.trigger("124",'incoming-message', input)
+  await pusherServer.trigger("124",'incoming-message', input)
   
   return  Response.json(({ status:200, success: true }))
 }
