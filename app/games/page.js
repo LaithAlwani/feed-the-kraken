d09bab5453a2f0c77b583b959861fc35478abe1b
laith <laithalwani@gmail.com>
2024-07-16 00:36:07 -0400

    adds loading and message if not active game rooms are available

--- app/games/page.js
+++ app/games/page.js
@@ -11,16 +11,23 @@ export default function GamesPage() {
   const { user } = useUser();
   const router = useRouter();
   const [gameRooms, setGameRooms] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const getGameRooms = async () => {
     const res = await fetch("/api/game");
-    if (res.ok) {
-      const data = await res.json();
-      setGameRooms([]);
-      data.forEach((gameRoom) => {
-        console.log(gameRoom);
-        setGameRooms((prev) => [...prev, gameRoom]);
-      });
+    try {
+      if (res.ok) {
+        const data = await res.json();
+        setGameRooms([]);
+        data.forEach((gameRoom) => {
+          console.log(gameRoom);
+          setGameRooms((prev) => [...prev, gameRoom]);
+        });
+      }
+    } catch (err) {
+      toast.err(err.message);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -46,7 +53,7 @@ export default function GamesPage() {
     pusherClient.bind("room-deleted", (gameRoom) => {
       getGameRooms();
     });
-    
+
     return () => {
       pusherClient.unsubscribe("lobby");
     };
@@ -57,19 +64,27 @@ export default function GamesPage() {
         <FaPlus />
       </Link>
       <h2>Active Games</h2>
-      <ul className="game-room-list">
-        {gameRooms.map((room) => (
-          <li key={room._id} className="game-room-list-item">
-            <h3>{room.name}</h3>{" "}
-            <p>
-              ({room.players.length}/11){" "}
-              <button onClick={() => joinRoom(room._id)} className="btn">
-                Join
-              </button>
-            </p>
-          </li>
-        ))}
-      </ul>
+      {loading ? (
+        <p>loading...</p>
+      ) : (
+        <ul className="game-room-list">
+          {gameRooms.length > 0 ? (
+            gameRooms.map((room) => (
+              <li key={room._id} className="game-room-list-item">
+                <h3>{room.name}</h3>{" "}
+                <p>
+                  ({room.players.length}/11){" "}
+                  <button onClick={() => joinRoom(room._id)} className="btn">
+                    Join
+                  </button>
+                </p>
+              </li>
+            ))
+          ) : (
+            <h3>No Active games available</h3>
+          )}
+        </ul>
+      )}
     </>
   );
 }
