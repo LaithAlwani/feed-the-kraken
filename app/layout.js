0f2e9e19f8985c4b34e03b387dc3e72e2f32e712
laith <laithalwani@gmail.com>
2024-07-24 10:19:11 -0400

    creates PWA, fixes hero styles

--- app/layout.js
+++ app/layout.js
@@ -7,9 +7,29 @@ import Navbar from "@/components/Navbar";
 
 
 export const metadata = {
-  title: "Feed the Kraken App",
+    title: "Feed the Kraken App",
   description: "A Companion app for Feed the Kraken boardgame",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "next14", "pwa", "next-pwa"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [
+    {
+      name: "imvinojanv",
+      url: "https://www.linkedin.com/in/imvinojanv/",
+    },
+  ],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "/kraken.png" },
+    { rel: "icon", url: "/kraken.png" },
+  ],
 };
+
+// export const metadata = {
+
+// };
 
 export default function RootLayout({ children }) {
   
--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,34 @@
 /** @type {import('next').NextConfig} */
+import withPWA from "next-pwa";
 const nextConfig = {
+  reactStrictMode: true, // Enable React strict mode for improved error handling
+  swcMinify: true, // Enable SWC minification for improved performance
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+  },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'funtails.de',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "funtails.de",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'img.clerk.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "img.clerk.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public", // destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // disable PWA in the development environment
+  register: true, // register the PWA service worker
+  skipWaiting: true, // skip waiting for service worker activation
+})(nextConfig);
+
+// export default nextConfig;
